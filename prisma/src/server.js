03609e09e7a61e9b4f3eee2a96ee26f8889c987f
Muhammad Furqan <furqan.b2002@gmail.com>
2025-10-22 17:48:23 +0500

    Graffana and Prometheus implmented

--- prisma/src/server.js
+++ prisma/src/server.js
@@ -4,15 +4,50 @@ require("dotenv").config()
 const express = require("express")
 const AuthorRoute = require("./routes/author-route")
 const BookRoute = require("./routes/book-route")
-
+const promClient = require("prom-client");
 
 const app = express()
-
 app.use(express.json())
+
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register });
+
+
+const httpRequestsCounter =new promClient.Counter({
+    name: "http_requests_total",
+    help: "Total number of HTTP requests",
+    labelNames: ["method", "route", "status"]
+});
+
+register.registerMetric(httpRequestsCounter);
+
+
+// Middleware to track  API requests
+
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        httpRequestsCounter.inc({
+            method: req.method,
+            route: req.path,
+            status: res.statusCode
+        });
+    });
+    next();
+})
+
+
+//Expose the /metrics endpoint for prometheus
+
+app.get('/metrics', async (req, res) => {
+    res.set("Content-Type", register.contentType);
+    res.end(await register.metrics())
+})
+
 
 
 app.use("/api/author", AuthorRoute)
 app.use("/api/book", BookRoute)
+
 
 
 app.listen(3000, () => {
