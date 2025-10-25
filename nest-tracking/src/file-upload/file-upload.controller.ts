a1595121d39463a523c2b9f224f57befc9c05df5
Muhammad Furqan <furqan.b2002@gmail.com>
2025-10-25 22:17:05 +0500

    Multer along cloudinary for image upload using nest js

---
+++ nest-tracking/src/file-upload/file-upload.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileUploadService } from './file-upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('file-upload')
+export class FileUploadController {
+  constructor(private readonly fileUploadService: FileUploadService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileUploadService.uploadFile(file);
+  }
+}
---
+++ nest-tracking/src/file-upload/file-upload.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { v2 as cloudinary } from 'cloudinary';
+import * as fs from 'fs';
+@Injectable()
+export class FileUploadService {
+  constructor(private prisma: PrismaService) {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
+
+  async uploadFile(file: Express.Multer.File) {
+    try {
+      const uploadResult = await this.uploadToCloudinary(file.path);
+      const newlySavedFile = await this.prisma.File.create({
+        data: {
+          filename: file.originalname,
+          publicId: uploadResult.public_id,
+          url: uploadResult.secure_url,
+        },
+      });
+      fs.unlinkSync(file.path);
+      return newlySavedFile;
+    } catch (e) {
+      console.log(e);
+      // remove in case of error
+      if (file.path && fs.existsSync(file.path)) {
+        fs.unlinkSync(file.path);
+      }
+      throw new InternalServerErrorException(
+        'File upload failed! Please try again after sometimeF',
+      );
+    }
+  }
+  private uploadToCloudinary(filePath: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      cloudinary.uploader.upload(filePath, (error, result) => {
+        if (result) {
+          resolve(result);
+        }
+        // reject(error);
+      });
+    });
+  }
+}
